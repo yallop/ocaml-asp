2f14f0c1d2a8ddc3d6dc4c15a67929afb9249821
Jeremy Yallop <yallop@gmail.com>
2021-02-10 21:51:19 +0000

    Add support for BER MetaOCaml N111.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,10 +5,13 @@ dispatch begin
   function
   | After_rules ->
      ocaml_lib "lib/asp";
-     flag ["ocaml"; "compile"; "open_print_code"] &
-     if Sys.ocaml_version = "4.04.0" then S[A"-open"; A"Print_code"]
-     else if Sys.ocaml_version = "4.07.1" then S[A"-open"; A"Codelib"]
-     else Printf.kprintf failwith "Unsupported OCaml version %s" Sys.ocaml_version
+     begin match Sys.ocaml_version with
+     | "4.04.0" ->
+        flag ["ocaml"; "compile"; "open_print_code"] & S[A"-open"; A"Print_code"]
+     | "4.07.1" | "4.11.1" ->
+        flag ["ocaml"; "compile"; "open_print_code"] & S[A"-open"; A"Codelib"]
+     | version ->
+        Printf.kprintf failwith "Unsupported OCaml version %s" version
+     end
   | _ -> ()
 end;;
-
